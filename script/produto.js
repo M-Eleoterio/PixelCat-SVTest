4321c8f0771859fafb07aa53e72667ccc1241e3f
M-Eleoterio <m.eleoteriovieira@gmail.com>
2023-05-30 17:27:20 -0300

    Site completo.

--- script/produto.js
+++ script/produto.js
@@ -1,3 +1,22 @@
+var stock = 10;
+var stockElement = document.querySelector('.product-stock');
+var quantidade = document.getElementById('quantity-input');
+var keyValues = [
+  'ABC12-DEFG3-HIJK4',
+  'LMNO5-PQRS6-TUVW7',
+  'XYZ89-12345-67890',
+  'MNPQ1-RSTU2-VWXY3',
+  'FGHI4-JKLM5-NOPQ6',
+  'UVWX7-YZ12A-345B6',
+  'CDEF7-GHIJ8-KLMN9',
+  'OPQR1-STUV2-WXYZ3',
+  '12345-67890-ABCDE',
+  'FGHIJ-KLMNO-PQRST',
+  'UVWXY-Z1234-56789',
+  'ABCDE-FGHIJ-KLMNO'
+];
+var currentIndex = 0;
+
 function changeImage(imagePath) {
     document.getElementById('main-image').src = imagePath;
 }
@@ -11,7 +30,6 @@ function decreaseQuantity() {
 }
 
 function increaseQuantity() {
-    var stock = 10;
     var quantityInput = document.getElementById('quantity-input');
     var currentQuantity = parseInt(quantityInput.value);
     if (currentQuantity < stock) {
@@ -23,7 +41,35 @@ function buy() {
     var quantityInput = document.getElementById('quantity-input');
     var quantity = parseInt(quantityInput.value);
     // Lógica para realizar a compra do produto
-    alert('Compra realizada com sucesso!');
+    if (stock > 0) {
+      stock-= quantityInput.value;
+      stockElement.textContent = 'Estoque: ' + stock + ' unidades';
+      var selectedKeys = [];
+      
+      for (var i = 0; i < quantity && i < keyValues.length; i++) {
+        if (currentIndex >= keyValues.length) {
+          currentIndex = 1;
+        }
+  
+        var key = keyValues[currentIndex];
+        selectedKeys.push(key);
+        currentIndex++;
+  
+        if (currentIndex >= keyValues.length) {
+          currentIndex = 0;
+        }
+      }
+      
+      for (var j = 0; j < selectedKeys.length; j++) {
+        alert('Key Steam: ' + selectedKeys[j]);
+        var indexToRemove = keyValues.indexOf(selectedKeys[j]);
+        keyValues.splice(indexToRemove, 1);
+      }
+      
+      quantityInput.value = 1;
+    } else {
+      alert('Produto sem estoque');
+    }
 }
 
 var header = document.getElementById('top');
@@ -44,3 +90,9 @@ window.addEventListener('scroll', function() {
   
   lastScrollPosition = currentScrollPosition;
 });
+
+
+
+
+
+
